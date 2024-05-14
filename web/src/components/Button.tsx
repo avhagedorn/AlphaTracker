f863a264bbe0b235f302c92b86ebe7702b67f065
Alan Hagedorn <avhagedorn@gmail.com>
2024-05-14 18:19:40 -0500

    💚 Fix button colors

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -2,19 +2,6 @@ import React from 'react';
 import "@/components/style/loading.css";
 
 interface ButtonProps {
-    onClick: () => void;
-    children: React.ReactNode;
-}
-
-export default function     Button({ 
-    children,
-    onClick = undefined,
-    isLoading = false,
-    disabled = false,
-    type = "button",
-    color = "emerald",
-    className = "",
- }: {
     children: React.ReactNode;
     onClick?: (() => void) | undefined;
     isLoading?: boolean;
@@ -22,21 +9,33 @@ export default function     Button({
     type?: "button" | "submit";
     color?: "emerald" | "red" | "blue" | "gray" | "yellow";
     className?: string;
- }) {
+}
 
-    let colorClass = `bg-${color}-500 hover:bg-${color}-700`;
-    if (disabled) {
-        colorClass = "bg-gray-300 cursor-not-allowed";
-    }
-    if (isLoading) {
-        colorClass = `bg-${color}-700 cursor-wait`;
+export default function Button({ 
+    children,
+    onClick = undefined,
+    isLoading = false,
+    disabled = false,
+    type = "button",
+    color = "emerald",
+    className = "",
+ }: ButtonProps) {
+
+    const getColors = () => {
+        if (disabled) {
+            return "bg-gray-300 cursor-not-allowed";
+        } else if (isLoading) {
+            return `bg-${color}-700 cursor-wait`;
+        } else {
+            return `bg-${color}-500 hover:bg-${color}-700`;
+        }
     }
 
     return (
         <button
-            className={`${className} ${colorClass} text-white font-bold py-2 px-4 rounded`}
+            className={`${className} ${getColors()} text-white font-bold py-2 px-4 rounded`}
             onClick={onClick}
-            disabled={isLoading || disabled}
+            disabled={disabled || isLoading}
             type={type}
         >
             {children} {isLoading && <span className="inline-loading"/>}
